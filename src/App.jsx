330ec47201ba352321d7e089e198393a725ff50a
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2025-04-07 11:34:22 +0530

    Update the Footer Links

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "./components/Footer/Footer";
+import FAQ from "./pages/Home/FAQ/FAQ";
+function App() {
+  return (
+    <>
+      <FAQ />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
