37b334d6f790b33294972511ca83ea08b8ad4407
unknown <sunilkhetri05@gamil.com>
2025-04-07 23:22:32 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Chat from './componants/Chat/Chat'
+import Graph from './componants/Graph/Graph'
+
+function App() {
+  return (
+    <>
+      <div>
+       <Chat/>
+       <Graph/>
+      </div>
+    </>
+    
+  )
+}
+
+export default App
