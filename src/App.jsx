767bccc572daffa2999f05edd534838ebf1057cf
Sushil321 <sushil.shivsharan009@gmail.com>
2025-04-07 23:09:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import CarousalOne from './componants/Home/Carousal/CarousalOne'
+
+
+import Solutionme from './pages/Solutions/Contractual&ProjectbasedHiring/ProjectBasedHiring'
+
+
+
+function App() {
+  return (
+    <>
+   
+   
+    
+    <CarousalOne/>
+    {/* <Solutionme/> */}
+     
+    
+    </>
+    
+  )
+}
+
+export default App
