40211283e466a68000307b0aee40332c3ac58421
Shadab786-developer <sq1597900@gmail.com>
2025-04-14 11:35:05 +0530

    Update the font size

--- src/pages/CenterofExce/COE1.jsx
+++ src/pages/CenterofExce/COE1.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function COE1() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className="text-[50px] text-[#0c0f6a] font-semilight text-center font-[Poppins]">
+      <h1 className="sm:text-[50px] text-[#0c0f6a] font-semilight text-center font-[Poppins]">
         VerbiQ's Center of Excellence
       </h1>
       <p className="sm:text-[30px] text-[20px] text-gray-600 text-center">
