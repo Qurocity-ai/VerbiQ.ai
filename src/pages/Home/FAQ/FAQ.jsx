5f4d5d4e8a807ea5998e15ed44c15e93c5e9efe3
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2025-04-17 17:40:15 +0530

    FAQ section is updated

--- src/pages/Home/FAQ/FAQ.jsx
+++ src/pages/Home/FAQ/FAQ.jsx
@@ -32,11 +32,11 @@ function FAQ() {
 
   return (
     <div className="flex flex-col items-end justify-center h-auto">
-      <div className="sm:grid sm:grid-cols-12 gap-4 flex flex-col items-center justify-center w-screen  sm:pr-38 sm:pl-72 py-34">
-        <div className="sm:col-span-5 sm:text-[52px] text-[22px] sm:font-normal text-[#0e128b] leading-[100%] flex justify-center w-full">
+      <div className="sm:grid sm:grid-cols-2 gap-4 flex flex-col items-center justify-center w-screen sm:pr-30 sm:pl-48 py-34">
+        <div className="sm:col-span-1 sm:text-[40px] text-[22px] sm:font-normal text-[#0e128b] leading-[100%] flex justify-center w-full">
           Frequently Asked <br className="sm:flex hidden" /> Questions
         </div>
-        <div className="sm:col-span-7 sm:pl-40 pl-3 pr-3 w-full">
+        <div className="sm:col-span-1 pl-3 pr-28 h-auto w-full">
           {faqData.map((item, index) => (
             <div key={index} className=" border-b border-b-gray-300 ">
               <button
@@ -48,7 +48,7 @@ function FAQ() {
                 aria-controls={`faq-answer-${index}`}
               >
                 <span
-                  className={`text-wrap flex justify-between items-center font-medium  w-full text-[20px] transition-all duration-700 ${
+                  className={`text-wrap flex justify-between items-center font-medium  w-full sm:text-[17px] text-[12px] transition-all duration-700 ${
                     activeIndex === index ? "text-[#161a85]" : "text-[#969696]"
                   }`}
                 >
@@ -66,7 +66,7 @@ function FAQ() {
                     viewBox="0 0 16 16"
                     data-supported-dps="16x16"
                     data-test-icon="add-small"
-                    className="sm:h-7 sm:w-7 h-5 w-5 "
+                    className="sm:h-6 sm:w-6 h-5 w-5 "
                   >
                     <path
                       d="M14 9H9v5H7V9H2V7h5V2h2v5h5z"
@@ -78,11 +78,13 @@ function FAQ() {
               </button>
               <div
                 id={`faq-answer-${index}`}
-                className={` transition-all duration-1000 ease-in-out overflow-hidden px-6 py-1 ${
+                className={` transition-all duration-1000 ease-in-out overflow-hidden px-6 ${
                   activeIndex === index ? "max-h-96" : "max-h-0"
                 }`}
               >
-                <p className="text-[#1015b4] opacity-100 ">{item.answer}</p>
+                <p className="text-[#1015b4] opacity-100 sm:text-[15px] text-[9px]  ">
+                  {item.answer}
+                </p>
               </div>
             </div>
           ))}
