fb3139f521e464d4958af9ab45ee89f21745fcc8
Kajaltiwari892 <kanjal55567@gmail.com>
2025-05-01 13:25:44 +0530

    fixed navbar

--- src/componants/Navbar/Navbar.jsx
+++ src/componants/Navbar/Navbar.jsx
@@ -20,41 +20,33 @@ export default function Navbar() {
     setShowResources(false);
   };
       
-  // Close dropdown when clicking outside
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (solutionsDropdownRef.current && !solutionsDropdownRef.current.contains(event.target)) {
         setShowSolution(false);
       }
-
       if (resourcesDropdownRef.current && !resourcesDropdownRef.current.contains(event.target)) {
         setShowResources(false);
       }
     };  
 
     document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
+    return () => document.removeEventListener('mousedown', handleClickOutside);
   }, []);
 
   return (
     <nav className={style.navbar}>
-      {/* Company Logo  */}
       <div className={style.logo}>
         <Link to="/">
           <img src="/assets/virbiqlogo.png" alt="Company Logo" />
         </Link>
       </div>
     
-      {/*  Hamburger icon only visible on small screens */}
       <div className={style.mobileMenuToggle} onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
         {isMobileMenuOpen ? <RxCross2 size={32} /> : <GiHamburgerMenu size={24} />}
       </div>
 
-      {/* Nav links with dropdown menus */}
       <div className={`${style.navlinks} ${isMobileMenuOpen ? style.showMenu : ''}`}>
-        {/* Solution with dropdown menu  */}
         <div 
           className={`${style.dropdownContainer} ${showSolution ? style.dropdownOpen : ''}`}
           ref={solutionsDropdownRef}
@@ -66,38 +58,35 @@ export default function Navbar() {
             <span>Solutions</span>
             <IoIosArrowForward className={`${style.dropdownArrow} ${showSolution ? style.arrowRotate : ''}`} />
           </div>
-          
-          {showSolution && (
-            <div className={`${style.dropdown} ${style.solutionsDropdown}`}>
-              <div className={style.row}>
-                <div className={style.item} onClick={() => { setShowSolution(false); setIsMobileMenuOpen(false); navigate("/fulltime")}}>
-                  <img src="/assets/FTHS.png" alt="Full Time" />
-                  <div className={style.itemContent}>
+                  {showSolution && (
+    <div className={`${style.dropdown} ${style.solutionsDropdown}`}>
+        <div className={style.row}>
+            <div className={style.item} onClick={() => { setShowSolution(false); setIsMobileMenuOpen(false); navigate("/fulltime")}}>
+                <img src="/assets/FTHS.png" alt="Full Time" />
+                <div className={style.itemContent}>
                     <div className={style.itemTitle}>
-                      <span>Full Time Hiring Support</span>
-                      <IoIosArrowForward className={style.itemArrow} />
+                        <span>Full Time Hiring Support</span>
                     </div>
-                  </div>
                 </div>
-                
-                <div className={style.item} onClick={() => { setShowSolution(false); setIsMobileMenuOpen(false); navigate("/contractual")}}>
-                  <img src="/assets/CAPBS.png" alt="Contractual" />
-                  <div className={style.itemContent}>
+            </div>
+            
+            <div className={style.item} onClick={() => { setShowSolution(false); setIsMobileMenuOpen(false); navigate("/contractual")}}>
+                <img src="/assets/CAPBS.png" alt="Contractual" />
+                <div className={style.itemContent}>
                     <div className={style.itemTitle}>
-                      <span>Contractual and Project based Support</span>
-                      <IoIosArrowForward className={style.itemArrow} />
+                        <span>Contractual and Project based Support</span>
                     </div>
-                  </div>
                 </div>
-              </div>
-              
-              <div className={style.row}>
-                <div className={style.item} onClick={() => {setShowSolution(false); setIsMobileMenuOpen(false); navigate("/language")}}>
-                  <img src="/assets/LangAss.png" alt="Language" />
+            </div>
+        </div>
+        
+        <div className={style.row}>
+            <div className={style.item} onClick={() => {setShowSolution(false); setIsMobileMenuOpen(false); navigate("/language")}}>
+                <img src="/assets/LangAss.png" alt="Language" />
+               
                   <div className={style.itemContent}>
                     <div className={style.itemTitle}>
                       <span>Language Assessments</span>
-                      <IoIosArrowForward className={style.itemArrow} />
                     </div>
                   </div>
                 </div>
@@ -107,7 +96,6 @@ export default function Navbar() {
                   <div className={style.itemContent}>
                     <div className={style.itemTitle}>
                       <span>Global Hiring Support</span>
-                      <IoIosArrowForward className={style.itemArrow} />
                     </div>
                   </div>
                 </div>
@@ -116,10 +104,8 @@ export default function Navbar() {
           )}
         </div>
         
-        {/* COE Link  */}
-        <Link onClick={() => setIsMobileMenuOpen(false)} to="/ceo">COE</Link>
+        <Link onClick={closeMobileMenu} to="/ceo">COE</Link>
         
-        {/* Resource with dropdown menu  */}
         <div
           className={`${style.dropdownContainer} ${showResources ? style.dropdownOpen : ''}`}
           ref={resourcesDropdownRef}
@@ -142,7 +128,6 @@ export default function Navbar() {
                 <div className={style.itemContent}>
                   <div className={style.itemTitle}>
                     <span>Global insight</span>
-                    <IoIosArrowForward className={style.itemArrow} />
                   </div>
                 </div>
               </div>
@@ -155,7 +140,6 @@ export default function Navbar() {
                 <div className={style.itemContent}>
                   <div className={style.itemTitle}>
                     <span>Blogs</span>
-                    <IoIosArrowForward className={style.itemArrow} />
                   </div>
                 </div>
               </div>
@@ -163,8 +147,7 @@ export default function Navbar() {
           )}
         </div>
 
-        {/* About Us Link */}
-        <Link onClick={() => setIsMobileMenuOpen(false)} to="/aboutUs">About Us</Link>
+        <Link onClick={closeMobileMenu} to="/aboutUs">About Us</Link>
       </div>
     </nav>
   );
