41f7a1c291f137eba67cc94b2203efc840dc8b31
Supriyasingh <supriyaexit@gmail.com>
2025-05-13 15:52:39 +0530

    add company dashboard

---
+++ src/componants/VerbiqPortal/CompanyDashboad/CompanyDashboard.jsx
@@ -0,0 +1,45 @@
+
+// import React from 'react'
+// import CompanySidebar from './CompanySidebar/CompanySidebar'
+// import PortalRoutes from '../../../routes/PortalRoutes'
+
+// function CompanyDashboard() {
+//   return (
+//    <div className="min-h-screen flex flex-col">
+//        <CompanySidebar  
+//         mobileOpen={mobileOpen} 
+//         setMobileOpen={setMobileOpen} 
+//         isSidebarOpen={isSidebarOpen}  />
+//         <div className="lg:pl-[272px] flex flex-col flex-1">
+       
+        
+//         <main className={`${styles.pageWrapper} flex-1 p-4 overflow-auto bg-white`}>
+//           <PortalRoutes />
+//         </main>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default CompanyDashboard
+import React, { useState } from 'react';
+import CompanySidebar from './CompanySidebar/CompanySidebar';
+import PortalRoutes from '../../../routes/PortalRoutes';
+import styles from './CompanySidebar/CompanySidebar.module.css';
+
+function CompanyDashboard() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <CompanySidebar isSidebarOpen={isSidebarOpen} />
+      <div className="lg:pl-[272px] flex flex-col flex-1">
+        <main className={`${styles.pageWrapper} flex-1 p-4 overflow-auto bg-white`}>
+          <PortalRoutes />
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default CompanyDashboard;
---
+++ src/portalpages/Clients/Teleperformance/Teleperformance.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Teleperformance() {
+  return (
+    <div>
+      Teleperformance
+    </div>
+  )
+}
+
+export default Teleperformance
