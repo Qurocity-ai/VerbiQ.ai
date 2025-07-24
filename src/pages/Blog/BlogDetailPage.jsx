df1a88dc38447e3725c34242cc8bca46d28fa2b2
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2025-07-24 15:32:41 +0530

    update the blog page

--- src/pages/Blog/BlogDetailPage.jsx
+++ src/pages/Blog/BlogDetailPage.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import styles from './BlogDetailPage.module.css'; 
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styles from "./BlogDetailPage.module.css";
 import config from "../../services/config";
-import { FourSquare } from 'react-loading-indicators';
+import { FourSquare } from "react-loading-indicators";
 
 const BlogDetailPage = () => {
   const { id } = useParams();
@@ -10,14 +10,14 @@ const BlogDetailPage = () => {
 
   useEffect(() => {
     fetch(`${config.apiUrl}/blog/blogs/${id}`)
-      .then(response => response.json())
-      .then(data => setBlog(data))
-      .catch(error => console.error('Error fetching blog:', error));
+      .then((response) => response.json())
+      .then((data) => setBlog(data))
+      .catch((error) => console.error("Error fetching blog:", error));
   }, [id]);
 
   // Function to format date as '21 Nov 2024'
   const formatDate = (dateString) => {
-    const options = { day: 'numeric', month: 'short', year: 'numeric' };
+    const options = { day: "numeric", month: "short", year: "numeric" };
     return new Date(dateString).toLocaleDateString(undefined, options);
   };
 
@@ -27,7 +27,11 @@ const BlogDetailPage = () => {
         <>
           <h1 className={styles.blogTitle}>{blog.title}</h1>
           <p className={styles.blogDate}>{formatDate(blog.date)}</p>
-          <img src={blog.imageUrl} alt={blog.title} className={styles.blogImage} />
+          <img
+            src={blog.imageUrl}
+            alt={blog.title}
+            className={styles.blogImage}
+          />
           <div className={styles.blogContent}>
             <div dangerouslySetInnerHTML={{ __html: blog.content }} />
           </div>
@@ -37,9 +41,9 @@ const BlogDetailPage = () => {
         </>
       ) : (
         <>
-        {/* // <p>Loading blog...</p> */}
-        
-        {/* <FourSquare color={["#a9b9c1", "#bda9c1", "#c1b1a9", "#adc1a9"]} /> */}
+          {/* // <p>Loading blog...</p> */}
+
+          {/* <FourSquare color={["#a9b9c1", "#bda9c1", "#c1b1a9", "#adc1a9"]} /> */}
         </>
       )}
     </div>
